5148aabe997cf7248e718b935dd5135e0dae3eab
Marya Zvereva <mashechka28@mail.ru>
2023-08-17 23:30:22 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 15_656;
+
+        int miles = price / 20;
+        System.out.println("Бонусных миль: " + miles);
+    }
+}
